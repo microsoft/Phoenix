a4746e52a267f8f616e294fb691985e9f3dfe483
SophiaMS <v-sowan@microsoft.com>
2016-03-17 10:35:31 -0700

    Commit latest test project
    Commit latest test project

---
+++ test/PhoneixTest/test1/TestCases/WAPTestBase.cs
@@ -0,0 +1,39 @@
+﻿namespace Phoenix.Test.UI.TestCases
+{
+    //using Phoenix.Test.UI.Framework.Configuration;
+    //using Phoenix.Test.UI.Framework.Extensions;
+    using Framework;
+    //using Framework.Models;
+    using System.Diagnostics;
+    //using Microsoft.Test.MaDLybZ;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using OpenQA.Selenium.Firefox;
+    using OpenQA.Selenium.IE;
+
+    [TestClass]
+    public abstract class WAPTestBase
+    {
+        private EventLog _eventLog;
+        //private FirefoxDriver driver;
+        public InternetExplorerDriver driver;
+
+        protected WAPTestBase()
+        {
+            _eventLog = new EventLog("Application");
+        }
+
+        [TestInitialize]
+        public virtual void TestInitialize()
+        {
+            //this.driver = new FirefoxDriver();
+            this.driver = new InternetExplorerDriver();
+        }
+
+        [TestCleanup]
+        public virtual void TestCleanup()
+        {
+            this.driver.Close();
+        }
+
+    }
+}
