0c70276d2e643c402583675f72bd333cd5adbda6
nasiva <v-nasiva@microsoft.com>
2016-04-26 23:18:34 +0000

    Web.config more updates after merge

--- CMP/Setup/SetupCMP/Helpers/SetupDatabaseHelper.cs
+++ CMP/Setup/SetupCMP/Helpers/SetupDatabaseHelper.cs
@@ -108,7 +108,8 @@ FROM [Microsoft.MgmtSvc.Store].[Config].[Settings] WHERE [Name] LIKE '%FQDN%' an
         static SetupDatabaseHelper()
         {
             // Create a random password. Sql DBs need a special character so adding that and a number just in case that's needed
-            SqlDbUserPassword = "!1" + Guid.NewGuid().ToString().Replace("-", "").Substring(0, 10);
+            //SqlDbUserPassword = "!1" + Guid.NewGuid().ToString().Replace("-", "").Substring(0, 10);
+            SqlDbUserPassword = "AzureRocksForReal!123";
             SetupInputs.Instance.EditItem(SetupInputTags.SqlDBAdminNameTag, SqlUsernameDuringInstall);
 
         }
